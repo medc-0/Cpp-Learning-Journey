a86cb0e40e8a9886c1fa0e250b799e5b49339dd7
medc-0 <moonisontop76@gmail.com>
2025-11-14 15:38:51 +0100

    feat: arithmetic operations learned

--- main.cpp
+++ main.cpp
@@ -57,6 +57,34 @@ int main() {
     std::cout << "You are: " << age << " years old" << "\n";
     */
 
+    // arithmetic operators = return the result of a specific
+    //                        arithmetic operation (+, -, *, /, %)
+
+    /*
+    int students = 20;
+
+    students = students + 1;
+    students += 1;
+    students++; (Incrementation often used within loops)
+
+    students = students - 1;
+    students -= 1;
+    students--; (Decrementation operator)
+
+    students = students * 2;
+    students *= 2;
+
+    students = students / 2;
+    students /= 2;
+
+    int remainder = students % 2;
+
+    parenthesis
+    multiplication & Division
+    addition & subtraction
+
+    std::cout << "Students: " << students;
+    */
 
     return 0; 
 }
