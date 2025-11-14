c9bb31aafd8057828c25eb103a56116d13b17bc9
medc-0 <moonisontop76@gmail.com>
2025-11-14 16:32:36 +0100

    docs: learned memory addresses

--- main.cpp
+++ main.cpp
@@ -499,6 +499,19 @@ int main() {
     }
     */
 
+    // memory address = a location in memory where data is stored
+    // a memory address can accessed with & (address-of operator)
+
+    /*
+    std::string name = "Med";
+    int age = 20;
+    bool student = true;
+
+    std::cout << &name << '\n';
+    std::cout << &age << '\n';
+    std::cout << &student << '\n';
+    */
+
     return 0; 
 }
 
