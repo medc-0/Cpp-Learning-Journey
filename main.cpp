2b0cc1766b9cb23af4bdab1807c8dd4340a47ee8
medc-0 <moonisontop76@gmail.com>
2025-11-23 15:54:14 +0100

    dcos: more practice with changing values with address

--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <vector>
 
-int main() {
+int main() 
+{
+    std::vector<std::string> animals = {
+        "dog", "cat", "bird"
+    };
 
-    std::vector<std::string> colors = {
-            "blue", "red", "green"
-    }; 
+    std::string animal1 = "lion";
+    std::string animal2 = "eagle";
+    std::string animal3 = "fish";
 
-    for (auto color : colors) {
-        std::cout << color << ", ";
+    animals.insert(animals.begin(), animal1);
+    animals.insert(animals.begin() + 3, animal2);
+    animals.insert(animals.end(), animal3);
+
+    for (auto& animal : animals) {
+        animal += " animal";
+        std::cout << animal << ", ";
     }
     std::cout << std::endl;
 
-    colors.insert(colors.begin(), "purple");
-    colors.insert(colors.begin() + 2, "grey");
-    colors.insert(colors.end(), "lime");
-
-    for (auto color : colors) {
-        std::cout << color << ", ";
-    }
-    std::cout << std::endl;
     return 0;
 }
