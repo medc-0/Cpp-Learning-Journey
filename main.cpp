5e71d494498b2a5b776aee45af37d6436edfd27e
medc-0 <moonisontop76@gmail.com>
2025-11-25 12:53:58 +0100

    docs: learned, more about cin

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 
 int main() {
 
-    std::string name;
-
-    std::cout << "Enter your name: ";
-    while(name.empty()) {
-        std::getline(std::cin >> std::ws, name);
+    int number;
+    std::cout << "Enter a number: ";
+    while (!(std::cin >> number)) {
+        std::cout << "Invalid number!\n";
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
     }
-    std::cout << "your name: " << name << '\n';
+    std::cout << "You entered: " << number; 
+
 
     return 0;
 }
