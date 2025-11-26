c741c71c10657bd0c359ce020502a63d94023485
medc-0 <moonisontop76@gmail.com>
2025-11-26 16:44:56 +0100

    docs: .

--- main.cpp
+++ main.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
-
-    // int arr[3] = {1, 2, 3};
-    int num1 = 10;
-    int num2 = 0;
-
-    try {   
-        int res = num1 / num2;
-        throw 505;
-    } catch (int errorCode) {
-        std::cout << "error" << errorCode <<'\n';
-    }
-
     return 0;
 }
