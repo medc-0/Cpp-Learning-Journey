f4f82a770c224e07f9eefef74518c018d7b8c26c
medc-0 <moonisontop76@gmail.com>
2025-11-28 17:08:53 +0100

    docs: learning, ctime library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
-#include <cmath>
+#include <ctime>
 
 int main() {
+
+    time_t now = time(nullptr);
+    std::cout << "Unix-timestamp: " << now << '\n';
+
+    tm* local = localtime(&now);
+
+    char buffer[50];
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", local);
+    std::cout << "Formatiertes Datum: " << buffer << "\n";
+
+    time_t passed = now + 10;
+
+    std::cout << "Waiting...\n";
+    std::cout << "Enter something:\n";
+    std::string dummy;
+    std::getline(std::cin, dummy);
+
+   time_t afterInput = time(nullptr);
+
+    if (afterInput > passed) {
+        std::cout << "The input was too LATE.\n";
+    } else {
+        std::cout << "The input was on time.\n";
+    }
     return 0;
 }
