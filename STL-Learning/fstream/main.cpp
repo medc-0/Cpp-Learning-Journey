16f5ad2d37e18c611b6a54e39f855fed25eb703e
medc-0 <moonisontop76@gmail.com>
2025-12-04 22:15:23 +0100

    docs: more basics to IO operations

--- STL-Learning/fstream/main.cpp
+++ STL-Learning/fstream/main.cpp
@@ -3,22 +3,47 @@
 #include <string>
 
 int main() {
+    /* Basic introduction to filestreams in C++ */
+    // Create and open file
+    std::ofstream MyFile("filename.txt");
+
+    // Write to the file
+    MyFile << "File operations can be complex, but fun.\n";
+
+    // Close the file
+    MyFile.close();
+
+    // Create a text string, which is used to output the text file
+    std::string MyLine;
+
+    // Read from the text file
+    std::ifstream MyReadFile("filename.txt");
+
+    // Use a while loop together with the getline() function to read the file line by line
+    while (std::getline(MyReadFile, MyLine)) {
+        std::cout << MyLine << std::endl;
+    }
+
+    // Close the file
+    MyReadFile.close();
+
 
     // Writing to a file
     std::ofstream outfile("example.txt");
     if (outfile.is_open()) {
-        outfile << "This is some plain text.\n";
-        outfile << "some more text.\n";
+        outfile << "Hello, World!\n";
+        outfile << "This is a test file.\n";
         outfile.close();
     } else {
         std::cerr << "Error opening file for writing!" << std::endl;
         return 1;
     }
 
+    // Reading from a file
     std::ifstream inFile("example.txt");
     std::string line;
     if (inFile.is_open()) {
-        while(std::getline(inFile, line)) {
+        while (std::getline(inFile, line)) {
             std::cout << line << std::endl;
         }
         inFile.close();
@@ -27,31 +52,7 @@ int main() {
         return 1;
     }
 
-    /* Practicing */
 
-    // Writing to a file
-    // std::ofstream outfile("example.txt");
-    // if (outfile.is_open()) {
-    //     outfile << "Hello, World!\n";
-    //     outfile << "This is a test file.\n";
-    //     outfile.close();
-    // } else {
-    //     std::cerr << "Error opening file for writing!" << std::endl;
-    //     return 1;
-    // }
-
-    // Reading from a file
-    // std::ifstream inFile("example.txt");
-    // std::string line;
-    // if (inFile.is_open()) {
-    //     while (std::getline(inFile, line)) {
-    //         std::cout << line << std::endl;
-    //     }
-    //     inFile.close();
-    // } else {
-    //     std::cerr << "Error opening file for reading!" << std::endl;
-    //     return 1;
-    // }
     
     return 0;
 }
