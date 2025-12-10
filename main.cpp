156989e60bc9aa39653c01b529a1b45e229d6fda
medc-0 <moonisontop76@gmail.com>
2025-12-10 23:01:51 +0100

    docs: password checker program written by me!

--- main.cpp
+++ main.cpp
@@ -1,14 +1,59 @@
 #include <iostream>
-#include <vector>
-#include <exception>
+#include <string>
 
-int main() {
-    std::vector<int> v = {1, 2, 3};
+int main()
+{
+    std::string password;
+    std::string passwordStrength;
+    bool hasUppercase = false; 
+    bool hasDigit = false;
+    bool hasSymbol = false;
+    int score = 0;
 
-    try {
-        std::cout << v.at(10) << '\n';
-    } catch(std::out_of_range e) {
-        std::cout << "Caught: \n\n\n\n\n\n\n\n\n\n\n\n" << e.what();
+    std::cout << "Enter a password: ";
+    std::getline(std::cin, password);
+
+     if (password.length() >= 8) 
+     {
+        for (int i=0; i < password.length(); i++) 
+        {
+            if (isdigit(password[i]))
+            {
+                hasDigit = true;
+            }
+            else if (password[i] == '&' || password[i] == '#' || password[i] == '@')
+            {   
+                hasSymbol = true;
+            }
+            else if (isupper(password[i])) {
+                hasUppercase = true;
+            }
+        }
+
+    if (hasDigit) score++;
+    if (hasUppercase) score++;
+    if (hasSymbol) score++;
+
+    if (score <= 1) 
+    {
+        passwordStrength = "weak";
+        std::cout << passwordStrength;
+
+    }
+    else if (score == 2)
+    {
+        passwordStrength = "medium";
+        std::cout << passwordStrength;
+    }
+    else
+    {
+        passwordStrength = "strong";
+        std::cout << passwordStrength;
+    }
+  }
+    else 
+    {
+        std::cout << "password cant be shorter than 8 characters\n";
     }
     return 0;
 }
