54d161563804872564ec574ad109bd3302a274c1
medc-0 <moonisontop76@gmail.com>
2025-12-11 17:06:58 +0100

    docs: todolist project done!

--- main.cpp
+++ main.cpp
@@ -1,59 +1,79 @@
 #include <iostream>
+#include <vector>
 #include <string>
+#include <limits>
 
-int main()
+void printMenu() 
 {
-    std::string password;
-    std::string passwordStrength;
-    bool hasUppercase = false; 
-    bool hasDigit = false;
-    bool hasSymbol = false;
-    int score = 0;
+    std::cout << "------------------\n";
+    std::cout << "| 1. Add task    |\n";
+    std::cout << "| 2. View tasks  |\n";
+    std::cout << "| 3. Remove task |\n";
+    std::cout << "| 4. Exit        |\n";
+    std::cout << "------------------\n";
+}
 
-    std::cout << "Enter a password: ";
-    std::getline(std::cin, password);
+int main() 
+{
+    std::vector<std::string> tasks;
+    int choice;
+    bool running = true;
 
-     if (password.length() >= 8) 
-     {
-        for (int i=0; i < password.length(); i++) 
+    std::cout << "--- Todo list ---\n";
+
+    while (running) 
+    {
+        printMenu();
+        std::cout << "Enter a choice (1-4): ";
+        std::cin >> choice;
+
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        if (choice == 1) 
         {
-            if (isdigit(password[i]))
+            std::string task;
+            std::cout << "Enter new task: ";
+            std::getline(std::cin, task);
+            tasks.push_back(task);
+            std::cout << "Added task << " << task << '\n'; 
+        }
+        else if (choice == 2) 
+        {
+            if (tasks.empty()) 
             {
-                hasDigit = true;
+                std::cout << "No tasks yet\n";
             }
-            else if (password[i] == '&' || password[i] == '#' || password[i] == '@')
-            {   
-                hasSymbol = true;
-            }
-            else if (isupper(password[i])) {
-                hasUppercase = true;
+            else {
+                for (int i = 0; i < tasks.size(); i++) 
+                {
+                    std::cout << i << ". " << tasks[i] << '\n';
+                }
             }
         }
+        else if (choice == 3) 
+        {
+            int index;
+            std::cout << "Enter task number to remove: ";
+            std::cin >> index;
 
-    if (hasDigit) score++;
-    if (hasUppercase) score++;
-    if (hasSymbol) score++;
-
-    if (score <= 1) 
-    {
-        passwordStrength = "weak";
-        std::cout << passwordStrength;
-
-    }
-    else if (score == 2)
-    {
-        passwordStrength = "medium";
-        std::cout << passwordStrength;
-    }
-    else
-    {
-        passwordStrength = "strong";
-        std::cout << passwordStrength;
-    }
-  }
-    else 
-    {
-        std::cout << "password cant be shorter than 8 characters\n";
+            if (index >= 0 && index < tasks.size())
+            {   
+                std::string removedTask = tasks[index];
+                tasks.erase(tasks.begin() + index);
+                std::cout << "Task Removed! " << '"' << removedTask << '"\n';
+            }
+            else 
+            {
+                std::cout << "Invalid task number!\n";
+            }
+        }
+        else if (choice == 4)
+        {   
+            std::cout << "--------------------\n";
+            std::cout << "| Exiting console..|\n";
+            std::cout << "--------------------\n";
+            running = false;
+        }
     }
     return 0;
 }
