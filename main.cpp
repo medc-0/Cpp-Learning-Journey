43562c7168de72f8977564f2fe83f41cca34e1b7
medc-0 <moonisontop76@gmail.com>
2025-12-14 15:53:33 +0100

    docs: learning more about vectors

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <vector>
 
 int main() 
-{   
-    std::cout << "Hello, World!" << '\n';
+{
+    std::vector<int> scores = {
+        10, 20, 30, 40, 50
+    };
+
+    for (auto score : scores) 
+    {
+        std::cout << score << '\n';
+    }
+
+    std::cout << "size: "<< scores.size() << '\n';
+    scores.clear();
+    
+    if (scores.empty()) 
+    {
+        std::cout << "no more scores left.\n";
+    }
+
     return 0;
 }
