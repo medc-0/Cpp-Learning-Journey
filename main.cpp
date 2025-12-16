694ada9129261bfe0e63bc7c12fc88b6d4c50591
medc-0 <moonisontop76@gmail.com>
2025-12-16 16:45:14 +0100

    docs: learning more about headers and strings

--- main.cpp
+++ main.cpp
@@ -1,18 +1,8 @@
 #include <iostream>
-// #include <vector>
 #include <string>
 
 int main() 
-{   
-    std::string word;
-
-    std::cout << "Enter a word: ";
-    std::getline(std::cin, word);
-
-    for (int i = word.length() -1; i < word.length(); i--)
-    {
-        std::cout << word[i];
-    }
-    std::cout << '\n';
-    return 0;
+{
+    std::string word = "Code";
+    std::cout << word << '\n';
 }
