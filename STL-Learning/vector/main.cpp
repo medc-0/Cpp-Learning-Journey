a7a30ad24441896301a6bc8fc7b17003172f466c
medc-0 <moonisontop76@gmail.com>
2025-12-19 14:56:28 +0100

    docs: looping through .at() vs [i]

--- STL-Learning/vector/main.cpp
+++ STL-Learning/vector/main.cpp
@@ -5,15 +5,16 @@ int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5};
 
     // good for debugging && bounds check but could loose performance
-    if(numbers.at(0) == 1) 
+    for (int i = 0; i < numbers.size(); ++i) 
     {
-        std::cout << "the number at index 0: " << numbers.at(0) << '\n';
+        std::cout << numbers.at(i) << ' ';
     }
-
+    std::cout << '\n';
+    
     // no bounds-check but good for performance
-    if (numbers[0] == 1) 
+    for (int i = 0; i < numbers.size(); ++i) 
     {
-        std::cout << "the number at index 0: " << numbers[0] << '\n';
+        std::cout << numbers[i] << ' ';
     }
     return 0;
 }
