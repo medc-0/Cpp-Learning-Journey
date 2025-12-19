901a79aa29d865828d89c72459c701e090fa168a
medc-0 <moonisontop76@gmail.com>
2025-12-19 14:51:04 +0100

    docs: comparing .at and [] indexing

--- STL-Learning/vector/main.cpp
+++ STL-Learning/vector/main.cpp
@@ -4,10 +4,16 @@
 int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5};
 
-    // good
-    std::cout << numbers.at(0) << '\n';
+    // good for debugging && bounds check but could loose performance
+    if(numbers.at(0) == 1) 
+    {
+        std::cout << "the number at index 0: " << numbers.at(0) << '\n';
+    }
 
-    // bad
-    std::cout << numbers[1] << '\n';
+    // no bounds-check but good for performance
+    if (numbers[0] == 1) 
+    {
+        std::cout << "the number at index 0: " << numbers[0] << '\n';
+    }
     return 0;
 }
