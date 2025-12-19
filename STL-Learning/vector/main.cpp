21a60fa2b6416ff2c5ae3206d55f72a2cb2a760a
medc-0 <moonisontop76@gmail.com>
2025-12-19 14:15:08 +0100

    docs: .at(i) vs [i]

--- STL-Learning/vector/main.cpp
+++ STL-Learning/vector/main.cpp
@@ -4,6 +4,10 @@
 int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5};
 
-    std::cout << 123 << '\n';
+    // good
+    std::cout << numbers.at(0) << '\n';
+
+    // bad
+    std::cout << numbers[1] << '\n';
     return 0;
 }
