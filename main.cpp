1e2ba0376c8380b00c93d205f2ec417b603b2b19
medc-0 <moonisontop76@gmail.com>
2025-12-20 17:01:50 +0100

    docs: learned more switch case-default statements

--- main.cpp
+++ main.cpp
@@ -1,37 +1,52 @@
 #include <iostream>
 
-int main()
+int main() 
 {
-    int dayNum;
+    int monthNum;
 
-    std::cout << "Enter a day (1-7): ";
-    std::cin >> dayNum;
+    std::cout << "Enter a number from (1-12): ";
+    std::cin >> monthNum;
 
-    switch(dayNum) 
+    switch(monthNum) 
     {
         case 1:
-            std::cout << "It is monday!\n";
+            std::cout << "It is Janurary!\n";
             break;
         case 2:
-            std::cout << "It is tuesday!\n";
+            std::cout << "It is feburary!\n";
             break;
         case 3:
-            std::cout << "It is wednesday!\n";
+            std::cout << "It is march!\n";
             break;
         case 4:
-            std::cout << "It is thursday!\n";
+            std::cout << "It is april!\n";
             break;
         case 5:
-            std::cout << "It is friday!\n";
+            std::cout << "It is may!\n";
             break;
         case 6:
-            std::cout << "It is saturday\n";
+            std::cout << "It is june!\n";
             break;
         case 7:
-            std::cout << "It is sunday!\n";
+            std::cout << "It is july!\n";
+            break;
+        case 8:
+            std::cout << "It is august!\n";
+            break;
+        case 9:
+            std::cout << "It is September!\n";
+            break;
+        case 10:
+            std::cout << "It is October!\n";
+            break;
+        case 11:
+            std::cout << "It is November!\n";
+            break;
+        case 12:
+            std::cout << "It is December!\n";
             break;
         default:
-            std::cout << "Invalid day!\n";
+            std::cout << "That is not a valid month!\n";
             break;
     }
     return 0;
