c74c8506be326fba18d0c61b7cf09cd8ea97f8a3
medc-0 <moonisontop76@gmail.com>
2025-12-20 17:33:53 +0100

    docs: learning, vectors with switches and better return codes

--- main.cpp
+++ main.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main() 
 {
-    char grade;
-    
-    std::cout << "Enter a grade char: ";
-    std::cin >> grade;
+    std::vector<std::string> items = {
+        "Sword", "shield", "potion"
+    };
+    std::vector<std::string> itemStats;
+    int itemNumber;
 
-    switch(grade) 
+    std::cout << "Pick a number from 1-3(4 to exit): ";
+    std::cin >> itemNumber;
+
+    switch(itemNumber) 
     {
-        case 'A':
-        case 'a':
-            std::cout << "Excellent A!\n";
+        case 1:
+            std::cout << items.at(0) << " +10 damage!\n";
+            itemStats.push_back("Sword +10 damage!");
             break;
-        case 'B':
-        case 'b':
-            std::cout << "Good B!\n";
+        case 2:
+            std::cout << items.at(1) <<" defense +10!\n";
+            itemStats.push_back("Shield defense +10!");
             break;
-        case 'C':
-        case 'c':
-            std::cout << "Average C!\n";
+        case 3:
+            std::cout << items.at(2) << " heals +50hp!\n";
+            itemStats.push_back("Potion heals +50hp");
             break;
-        case 'D':
-        case 'd':
-            std::cout << "Poor D!\n";
+        case 4:
+            std::cout << "Exiting program..\n";
             break;
-        case 'F':
-        case 'f':
-            std::cout << "Fail F!" << "\n";
-            break;
+            return EXIT_SUCCESS;
         default:
-            std::cout << "Enter a valid grade!";
+            std::cout << "Pick a valid item number!\n";
             break;
+    }
+
+    for (auto item : items) 
+    {
+        std::cout << item << '\n';
     }
     return 0;
 }
