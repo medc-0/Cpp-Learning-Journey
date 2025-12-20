f35eeb11112e484c8dd39d40212f81f7da47cd1f
medc-0 <moonisontop76@gmail.com>
2025-12-20 17:16:51 +0100

    dosc: learning chaining with switch case statements

--- main.cpp
+++ main.cpp
@@ -2,51 +2,35 @@
 
 int main() 
 {
-    int monthNum;
+    char grade;
+    
+    std::cout << "Enter a grade char: ";
+    std::cin >> grade;
 
-    std::cout << "Enter a number from (1-12): ";
-    std::cin >> monthNum;
-
-    switch(monthNum) 
+    switch(grade) 
     {
-        case 1:
-            std::cout << "It is Janurary!\n";
+        case 'A':
+        case 'a':
+            std::cout << "Excellent A!\n";
             break;
-        case 2:
-            std::cout << "It is feburary!\n";
+        case 'B':
+        case 'b':
+            std::cout << "Good B!\n";
             break;
-        case 3:
-            std::cout << "It is march!\n";
+        case 'C':
+        case 'c':
+            std::cout << "Average C!\n";
             break;
-        case 4:
-            std::cout << "It is april!\n";
+        case 'D':
+        case 'd':
+            std::cout << "Poor D!\n";
             break;
-        case 5:
-            std::cout << "It is may!\n";
-            break;
-        case 6:
-            std::cout << "It is june!\n";
-            break;
-        case 7:
-            std::cout << "It is july!\n";
-            break;
-        case 8:
-            std::cout << "It is august!\n";
-            break;
-        case 9:
-            std::cout << "It is September!\n";
-            break;
-        case 10:
-            std::cout << "It is October!\n";
-            break;
-        case 11:
-            std::cout << "It is November!\n";
-            break;
-        case 12:
-            std::cout << "It is December!\n";
+        case 'F':
+        case 'f':
+            std::cout << "Fail F!" << "\n";
             break;
         default:
-            std::cout << "That is not a valid month!\n";
+            std::cout << "Enter a valid grade!";
             break;
     }
     return 0;
