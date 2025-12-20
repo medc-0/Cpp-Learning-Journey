ef076c4777884937d0d8e8500168b690340be111
medc-0 <moonisontop76@gmail.com>
2025-12-20 16:48:40 +0100

    docs: simple more, practice with switches

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
-int main() 
+int main()
 {
+    int dayNum;
+
+    std::cout << "Enter a day (1-7): ";
+    std::cin >> dayNum;
+
+    switch(dayNum) 
+    {
+        case 1:
+            std::cout << "It is monday!\n";
+            break;
+        case 2:
+            std::cout << "It is tuesday!\n";
+            break;
+        case 3:
+            std::cout << "It is wednesday!\n";
+            break;
+        case 4:
+            std::cout << "It is thursday!\n";
+            break;
+        case 5:
+            std::cout << "It is friday!\n";
+            break;
+        case 6:
+            std::cout << "It is saturday\n";
+            break;
+        case 7:
+            std::cout << "It is sunday!\n";
+            break;
+        default:
+            std::cout << "Invalid day!\n";
+            break;
+    }
     return 0;
-}   
+}
