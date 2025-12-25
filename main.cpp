9d8bdd8c8aef59ef2155d2003cd5d70c1dd33d24
medc-0 <moonisontop76@gmail.com>
2025-12-25 18:36:42 +0100

    fix: wrong file, easy output

--- main.cpp
+++ main.cpp
@@ -1,28 +1,7 @@
 #include <iostream>
 
-int main() 
+auto main() -> int
 {
-    int vec3d[2][2][2] = {
-        { 
-            {1, 2},
-            {3, 4}
-        },
-        { 
-            {5, 6},
-            {7, 8}
-        }
-    };
-
-    for (int i = 0; i < 2; i++) 
-    {
-        for (int j = 0; j < 2; j++) 
-        {
-            for (int  k = 0; k < 2; k++) 
-            {
-                std::cout << vec3d[i][j][k] << " ";
-            }
-            std::cout << '\n';
-        }
-    }
+    std::cout << "Hello, World!\0" << '\n';
     return 0;
 }
