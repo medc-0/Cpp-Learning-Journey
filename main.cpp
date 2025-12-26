92c01256ee0bb7aa98f3a2077c8eb22d7fc9d059
medc-0 <moonisontop76@gmail.com>
2025-12-26 18:15:36 +0100

    docs: amount checking with if statment

--- main.cpp
+++ main.cpp
@@ -10,6 +10,11 @@ int main() {
     std::cout << "Enter how much damage to deal: ";
     std::cin >> damageAmount;
 
-    attack(damageAmount);
+    if (damageAmount <= 100 && damageAmount >= 1) {
+        attack(damageAmount);
+    }
+    else {
+        std::cout << "Invalid amount of damage: max(100)\n";
+    }
     return 0;
 }
