da64987b99f8b9a03f2c13433740d836551c05ad
medc-0 <moonisontop76@gmail.com>
2025-12-27 10:46:38 +0100

    docs: learning with pointers and referencing

--- main.cpp
+++ main.cpp
@@ -1,47 +1,10 @@
 #include <iostream>
 
-void attackEnemy(int &enemyHealth, int damage) {
-    enemyHealth -= damage;
-}
-
-void healHero(int &heroHealth, int healAmount) {
-    heroHealth += healAmount;
-}
-
-void battleSummary(const int heroHealth, const int enemyHealth) {
-    std::cout << "Hero\'s health: " << heroHealth << '\n';
-    std::cout << "Enemy\'s health: " << enemyHealth << '\n';
-
-    if (heroHealth > enemyHealth) {
-        std::cout << "Hero has won!\n";
-    }
-    else {
-        std::cout << "enemy has won..\n";
-    }
-}
-
-
 int main() {
-    int heroHealth;
-    int heroDamage;
-    int heroHeal;
-    int enemyHealth;
+    int coins = 100;
+    int* ptrCoins = &coins;
 
-    std::cout << "Enter heros starting health: ";
-    std::cin >> heroHealth;
-
-    std::cout << "Enter enemys starting health: ";
-    std::cin >> enemyHealth;
-
-    std::cout << "How much damage does the hero want to deal: ";
-    std::cin >> heroDamage;
-    attackEnemy(enemyHealth, heroDamage);
-
-    std::cout << "Enter how much the hero should be healed: ";
-    std::cin >> heroHeal;
-    healHero(heroHealth, heroHeal);
-
-    std::cout << "Sumarizing battle...\n";
-    battleSummary(heroHealth, enemyHealth);
+    std::cout << "Address: " << ptrCoins << '\n';
+    std::cout << "Value: " << *ptrCoins << '\n';
     return 0;
 }
