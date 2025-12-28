72f94a741367d8d4bc3fbf646e35576a2b9abf0a
medc-0 <moonisontop76@gmail.com>
2025-12-28 18:55:06 +0100

    docs: learned how to work with the heap, and allocate memory, using a vector of pointers and learning how to access these members

--- main.cpp
+++ main.cpp
@@ -1,25 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-#define a using
-#define b namespace
-#define c std
-#define d ;
-#define e int
-#define f main
-#define g (
-#define h )
-#define i {
-#define j }
-#define k cout
-#define l <<
-#define m "Hello World!"
-#define n endl
-#define o return
-#define p 0
+// define a struct weapon
+struct Weapon {
+    std::string name;
+    int damage;
+};
 
-a b c d
+int main() {
+    // Create 3 weapon variables on the heap
+    Weapon* sword = new Weapon{"Sword", 25};
+    Weapon* bow = new Weapon{"Bow", 15};
+    Weapon* axe = new Weapon{"Axe", 35};
 
-e f g h i
-    k l m l n d
-    o p d
-j
+    // Store pointers to the std::vector<Weapon*>
+    std::vector<Weapon*> weapons;
+    weapons.push_back(sword);
+    weapons.push_back(bow);
+    weapons.push_back(axe);
+
+    // print all weapons through a for-loop using the pointers
+    for (Weapon* w : weapons) {
+        std::cout << "Weapon: " << w->name << " | Damage: " << w->damage << '\n';
+    }
+
+    // find the weapon sword and increase it damage by +10
+    for (Weapon* w : weapons) {
+        if (w->name == "Sword") {
+            w->damage += 10;
+        }
+        std::cout << "Weapon: " << w->name << " | Damage: " << w->damage << '\n';
+    }
+
+    // clean up vector from heap manually
+    for (Weapon*& w : weapons) {
+        delete w;
+        w = nullptr;
+    }
+    weapons.clear();
+    return 0;
+}
