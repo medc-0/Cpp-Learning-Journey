12473f94e179af8a4628594578272e888fdbe1c5
medc-0 <moonisontop76@gmail.com>
2025-12-28 19:34:03 +0100

    docs: working with pointers in vectors and variables created manually in the heap

--- main.cpp
+++ main.cpp
@@ -1,43 +1,40 @@
 #include <iostream>
 #include <vector>
-#include <string>
-
-// define a struct weapon
-struct Weapon {
-    std::string name;
-    int damage;
-};
 
 int main() {
-    // Create 3 weapon variables on the heap
-    Weapon* sword = new Weapon{"Sword", 25};
-    Weapon* bow = new Weapon{"Bow", 15};
-    Weapon* axe = new Weapon{"Axe", 35};
+    double* temp1 = new double(23.5);
+    double* temp2 = new double(28.2);
+    double* temp3 = new double(19.8);
+    double* temp4 = new double(31.0);
+    double* temp5 = new double(26.7);
 
-    // Store pointers to the std::vector<Weapon*>
-    std::vector<Weapon*> weapons;
-    weapons.push_back(sword);
-    weapons.push_back(bow);
-    weapons.push_back(axe);
+    std::vector<double*> temperatures;
+    temperatures.push_back(temp1);
+    temperatures.push_back(temp2);
+    temperatures.push_back(temp3);
+    temperatures.push_back(temp4);
+    temperatures.push_back(temp5);
 
-    // print all weapons through a for-loop using the pointers
-    for (Weapon* w : weapons) {
-        std::cout << "Weapon: " << w->name << " | Damage: " << w->damage << '\n';
+    for (double* t : temperatures) {
+        std::cout << "Temperature C: " << *t << '\n';
     }
 
-    // find the weapon sword and increase it damage by +10
-    for (Weapon* w : weapons) {
-        if (w->name == "Sword") {
-            w->damage += 10;
+    double* largestVal = temperatures[0];
+    double total = 0.0;
+    for (double* t : temperatures) {
+        total += *t;
+        if (*t > *largestVal) {
+            largestVal = t;
         }
-        std::cout << "Weapon: " << w->name << " | Damage: " << w->damage << '\n';
     }
-
-    // clean up vector from heap manually
-    for (Weapon*& w : weapons) {
-        delete w;
-        w = nullptr;
+    std::cout << *largestVal << '\n';
+    double average = total / temperatures.size();
+    std::cout << "average: " << average << '\n';
+    
+    for (double*& t : temperatures) {
+        delete t;
+        t = nullptr;
     }
-    weapons.clear();
+    temperatures.clear();
     return 0;
 }
