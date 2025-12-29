a437f8419895a5eb5941a2fdad06841d07a16a8b
medc-0 <moonisontop76@gmail.com>
2025-12-29 10:10:00 +0100

    docs: learning raw arrays with pointers

--- main.cpp
+++ main.cpp
@@ -1,40 +1,23 @@
 #include <iostream>
-#include <vector>
 
-int main() {
-    double* temp1 = new double(23.5);
-    double* temp2 = new double(28.2);
-    double* temp3 = new double(19.8);
-    double* temp4 = new double(31.0);
-    double* temp5 = new double(26.7);
+int main()
+{
+    int potions[5] = {
+        2, 5, 0, 10, 7};
+    int *firstPot = potions; // first index
 
-    std::vector<double*> temperatures;
-    temperatures.push_back(temp1);
-    temperatures.push_back(temp2);
-    temperatures.push_back(temp3);
-    temperatures.push_back(temp4);
-    temperatures.push_back(temp5);
-
-    for (double* t : temperatures) {
-        std::cout << "Temperature C: " << *t << '\n';
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << "number: " << *(firstPot + i) << '\n';
     }
 
-    double* largestVal = temperatures[0];
-    double total = 0.0;
-    for (double* t : temperatures) {
-        total += *t;
-        if (*t > *largestVal) {
-            largestVal = t;
-        }
+    *(firstPot + 1) += 3;
+    *(firstPot + 4) -= 2;
+
+    std::cout << "******** After Modification ********\n";
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << "number: " << *(firstPot + i) << '\n';
     }
-    std::cout << *largestVal << '\n';
-    double average = total / temperatures.size();
-    std::cout << "average: " << average << '\n';
-    
-    for (double*& t : temperatures) {
-        delete t;
-        t = nullptr;
-    }
-    temperatures.clear();
     return 0;
 }
