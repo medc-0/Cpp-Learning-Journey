f16d6b46d8237550f8b1e912f2066e5dc4ecb813
medc-0 <moonisontop76@gmail.com>
2025-12-29 10:24:03 +0100

    docs: more practice with raw arrays and pointers

--- main.cpp
+++ main.cpp
@@ -3,21 +3,21 @@
 int main()
 {
     int potions[5] = {
-        2, 5, 0, 10, 7};
-    int *firstPot = potions; // first index
+        2, 5, 0, 10, 7
+    };
+    std::string potNames[5] = {
+        "Health", "Mana", "Stamina", "Strength", "Speed"
+    };
+    int* firstPot = potions;
+    std::string* firstPotN = potNames;
 
-    for (int i = 0; i < 5; i++)
-    {
-        std::cout << "number: " << *(firstPot + i) << '\n';
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Potion Name: " << *(firstPotN + i) << " | Potion Amount: " << *(firstPot + i) << '\n';
+        *(firstPot + i) += 3; // increase all potions counts by 3
     }
-
-    *(firstPot + 1) += 3;
-    *(firstPot + 4) -= 2;
-
-    std::cout << "******** After Modification ********\n";
-    for (int i = 0; i < 5; i++)
-    {
-        std::cout << "number: " << *(firstPot + i) << '\n';
+    std::cout << "**** After modifications ****\n";
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Potion Name: " << *(firstPotN + i) << " | Potion Amount: " << *(firstPot + i) << '\n';
     }
-    return 0;
+    rdeturn 0;
 }
