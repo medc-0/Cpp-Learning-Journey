5e2077ab0b21818085594c71e4f951718bbbcc0a
medc-0 <moonisontop76@gmail.com>
2025-12-30 10:58:53 +0100

    docs: advanced usage of structs and constructor initilizer list, last functions left to write [#1]

--- main.cpp
+++ main.cpp
@@ -1,19 +1,50 @@
 #include <iostream>
 
-struct Person {
+struct Entity {
     std::string name;
-    int age;
+    int speed;
+    int health;
+    double attackDamage;
 
-    Person(const std::string& n, int a)
-    : name(n), age(a) {}
+    Entity(const std::string& n, int s, int h, double attackDamage)
+    : name(n), speed(s), health(h), attackDamage(attackDamage) {}
 
-    void printInfo() const {
-        std::cout << "Name: " << name << " | Age: " << age << '\n';
+    void printStats() const {
+        std::cout << "Name: " << name << '\n' << "Speed: " << speed << 
+        '\n' << "Health: " << health << '\n' << "Attack Damage: " << attackDamage << '\n';
+    }
+
+    void attackPlayer() {
+
+    }
+};
+
+struct Player {
+    std::string name;
+    int speed;
+    int health;
+    double attackDamage;
+
+    Player(const std::string& n, int s, int h, double attackDamage)
+    : name(n), speed(s), health(h), attackDamage(attackDamage) {}
+
+    void printPlayerStats() const {
+        std::cout << "Name: " << name << '\n' << "Speed: " << speed <<
+        '\n' << "Health: " << health << '\n' << "Attack Damage: " << attackDamage << '\n';
+    }
+
+    void attackEntity() {
+
     }
 };
 
 int main() {
-    Person p("Bob", 20);
-    p.printInfo();
+    Entity e("Orc", 2, 150, 10.0);
+    e.printStats();
+    e.attackPlayer();
+
+    Player p("Bob", 4, 100, 5.0);
+    p.printPlayerStats();
+    p.attackEntity();
     return 0;
 }
