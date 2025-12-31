9f77f546d92ff63f29f1943b04a92afd5b021094
medc-0 <moonisontop76@gmail.com>
2025-12-31 15:01:11 +0100

    refactor: program with much cleaner and better OOP and inheritance

--- main.cpp
+++ main.cpp
@@ -1,58 +1,48 @@
 #include <iostream>
 
-struct Entity {
+struct Character {
     std::string name;
     int speed;
     int health;
     double attackDamage;
+    bool isAlive = true;
 
-    Entity(const std::string& n, int s, int h, double attackDamage)
-    : name(n), speed(s), health(h), attackDamage(attackDamage) {}
+    Character(const std::string& n, int s, int h, double attackDamage, bool isAlive)
+    : name(n), speed(s), health(h), attackDamage(attackDamage), isAlive(isAlive) {}
 
     void printStats() const {
-        std::cout << "Name: " << name << '\n' << "Speed: " << speed << 
-        '\n' << "Health: " << health << '\n' << "Attack Damage: " << attackDamage << '\n';
+        std::cout << "Name: " << name << '\n'
+                  << "HP: " << health << '\n'
+                  << "DMG: " << attackDamage << '\n';
     }
 
-    void attackPlayer(int& health) {
-        health -= attackDamage;
+    void attackEntity(Character& other) {
+        other.health -= attackDamage;
     }
 };
+struct Entity : Character {
+    using Character::Character;
+};
 
-struct Player {
-    std::string name;
-    int speed;
-    int health;
-    double attackDamage;
-
-    Player(const std::string& n, int s, int h, double attackDamage)
-    : name(n), speed(s), health(h), attackDamage(attackDamage) {}
-
-    void printPlayerStats() const {
-        std::cout << "Name: " << name << '\n' << "Speed: " << speed <<
-        '\n' << "Health: " << health << '\n' << "Attack Damage: " << attackDamage << '\n';
-    }
-
-    void attackEntity(int& health) {
-        health -= attackDamage;
-    }
+struct Player : Character {
+    using Character::Character;
 };
 
 int main() {
-    Entity e("Orc", 2, 150, 10.0);
-    Player p("Bob", 4, 100, 5.0);
+    Entity e("Orc", 2, 150, 10.0, true);
+    Player p("Bob", 4, 100, 5.0, true);
     
     std::cout << e.name << " - ***Stats***\n";
     e.printStats();
 
     std::cout << "The " << e.name << " is attacking " << p.name << " with a mace!\n";
-    e.attackPlayer(p.health);
+    e.attackEntity(p);
 
     std::cout << p.name << " took damage and now players stats: \n";
-    p.printPlayerStats();
+    p.printStats();
 
     std::cout << p.name << " is attacking " << e.name << " with a Fire Sword!\n";
-    e.attackPlayer(e.health);
+    e.attackEntity(e);
     
     std::cout << "The " << e.name << " took damage and now enemies stats: \n";
     e.printStats();
