a830c6fd5af39c29a523420c705779c82b521f7b
medc-0 <moonisontop76@gmail.com>
2026-01-01 15:36:58 +0100

    docs: more practice with structs and nested structs aswell as making a points check inside rectangles

--- main.cpp
+++ main.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
-#include <vector>
+#include <cmath>
 
-struct Particle {
-    float x, y;
-    float vx, vy;
-    double mass;
+struct Point {
+    double x, y;
+};
 
-    Particle(float x, float y, float vx, float vy, double mass)
-        : x(x), y(y), vx(vx), vy(vy), mass(mass) {}
+struct Rectangle {
+    Point topLeft;
+    Point bottomRight;
 
-    void move(double dt) {
-        x += vx * dt;
-        y += vy * dt;
+    double area() const {
+        double width = std::abs(bottomRight.x - topLeft.x);
+        double height = std::abs(topLeft.y - bottomRight.y);
+        return width * height;
     }
 
-    void printPos() const {
-        std::cout << "Pos=(" << x << ", " << y << ") Vel=(" << vx << ", " << vy << ")\n";
+    bool contains(const Point& p) const {
+        return (p.x >= topLeft.x && p.x <= bottomRight.x &&
+                p.y <= topLeft.y && p.y >= bottomRight.y);
     }
 };
 
 int main() {
-    Particle particle1(8.2, 1.2, 5.32, 5.85, 100.20);
-    Particle particle2(4.1, 5.8, 3.33, 8.55, 120.9);
-    Particle particle3(12.0, 3.5, 4.35, 9.21, 150.11);
+    Rectangle r1{{0,5}, {4,0}};
+    Rectangle r2{{1,3}, {3,1}};
 
-    std::vector<Particle> particles = {particle1, particle2, particle3};
+    Point p1{2,2};
+    Point p2{5,5};
 
-    for (Particle& p : particles) {
-        p.move(0.1);
-        p.printPos();
-    }
+    std::cout << "R1 area: " << r1.area() << "\n";
+    std::cout << "R2 area: " << r2.area() << "\n";
+
+    std::cout << "P1 inside R1? " << (r1.contains(p1) ? "Yes" : "No") << '\n';
+    std::cout << "P2 inside R2? " << (r2.contains(p2) ? "Yes" : "No") << '\n';
     return 0;
 }
