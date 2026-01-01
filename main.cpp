1f94df7195657f19caffc3ace0b8cf214c81abdc
medc-0 <moonisontop76@gmail.com>
2026-01-01 20:44:54 +0100

    docs: Vec2 mini example complete and learned  static member functions

--- main.cpp
+++ main.cpp
@@ -1,57 +1,80 @@
 #include <iostream>
 #include <cmath>
 
-struct Vec2 {
+struct Vec2
+{
     double x, y;
 
     Vec2(double x, double y)
-    : x(x), y(y) {}
+        : x(x), y(y) {}
 
-    double length() const {
-        return std::sqrt(x*x + y*y);
+    double length() const
+    {
+        return std::sqrt(x * x + y * y);
     }
 
-    void add(const Vec2& other) {
+    void add(const Vec2 &other)
+    {
         x += other.x;
         y += other.y;
     }
 
-    void subtract(const Vec2& other) {
+    void subtract(const Vec2 &other)
+    {
         x -= other.x;
         y -= other.y;
     }
 
-    void scale(double s) {
+    void scale(double s)
+    {
         x *= s;
         y *= s;
     }
 
-    Vec2 added(const Vec2& other) const {
-        return Vec2 {other.x + x, other.y + y};
+    double dot(const Vec2& other) const
+    {
+        return x * other.x + y * other.y;
     }
 
-    Vec2 subtracted(const Vec2& other) const {
+    void print() const
+    {
+        std::cout << "(" << x << ", " << y << ")\n";
+    }
+
+    static Vec2 zero() {
+        return Vec2{0, 0};
+    }
+
+    Vec2 added(const Vec2 &other) const
+    {
+        return Vec2{other.x + x, other.y + y};
+    }
+
+    Vec2 subtracted(const Vec2 &other) const
+    {
         return Vec2{x - other.x, y - other.y};
     }
 
-    Vec2 scaled(double s) const {
+    Vec2 scaled(double s) const
+    {
         return Vec2{x * s, y * s};
     }
 
-    void print() const {
-        std::cout << "(" << x << ", " << y << ")\n";
+    Vec2 normalized() const
+    {
+        double len = length();
+        if (len == 0) {
+            return Vec2::zero();
+        }
+        return Vec2{x / len, y / len};
     }
 };
 
-int main() {
-    Vec2 a(3, 4);
-    Vec2 b(1, 2);
-
-    Vec2 c = a.added(b); // New Vec2 (4, 6)
-    Vec2 d = c.scaled(0.5); // New Vec2 (2, 3)
-
-    a.print();
-    c.print();
-    d.print();
+int main()
+{
+    Vec2 z = Vec2::zero();
+    Vec2 a(2, 2);
+    z.length();
+    z.print();
     return 0;
 }
