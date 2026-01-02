f5e7593dc74c4229543a2a46a857305eddff350b
medc-0 <moonisontop76@gmail.com>
2026-01-02 10:38:51 +0100

    docs: last structs exercise fully learned and understood structs, data, methods, const correctness, static member functions and more!

--- main.cpp
+++ main.cpp
@@ -1,80 +1,72 @@
 #include <iostream>
 #include <cmath>
 
-struct Vec2
-{
-    double x, y;
+struct Vec2 {
+    double x;
+    double y;
 
     Vec2(double x, double y)
-        : x(x), y(y) {}
+    : x(x), y(y) {}
 
-    double length() const
-    {
-        return std::sqrt(x * x + y * y);
+    double length() const {
+        return std::sqrt(x*x + y*y);
     }
 
-    void add(const Vec2 &other)
-    {
-        x += other.x;
-        y += other.y;
-    }
-
-    void subtract(const Vec2 &other)
-    {
-        x -= other.x;
-        y -= other.y;
-    }
-
-    void scale(double s)
-    {
-        x *= s;
-        y *= s;
-    }
-
-    double dot(const Vec2& other) const
-    {
-        return x * other.x + y * other.y;
-    }
-
-    void print() const
-    {
-        std::cout << "(" << x << ", " << y << ")\n";
-    }
-
-    static Vec2 zero() {
-        return Vec2{0, 0};
-    }
-
-    Vec2 added(const Vec2 &other) const
-    {
-        return Vec2{other.x + x, other.y + y};
-    }
-
-    Vec2 subtracted(const Vec2 &other) const
-    {
-        return Vec2{x - other.x, y - other.y};
-    }
-
-    Vec2 scaled(double s) const
-    {
+    Vec2 scaled(double s) const {
         return Vec2{x * s, y * s};
     }
 
-    Vec2 normalized() const
-    {
-        double len = length();
-        if (len == 0) {
-            return Vec2::zero();
-        }
-        return Vec2{x / len, y / len};
+    static Vec2 zero() {
+        return Vec2{0,0};
+    }
+
+};
+
+struct Mover {
+    Vec2 position;
+    Vec2 velocity;
+
+    Mover(Vec2 position, Vec2 velocity)
+    : position(position), velocity(velocity) {}
+
+    void step(double dt) {
+        position.x = position.x + velocity.x * dt;
+        position.y = position.y + velocity.y * dt;
+    }
+
+    double speed() const {
+        return velocity.length();
+    }
+
+    void print() const {
+        std::cout << "Pos(" << position.x << ", " << position.y << ") "
+                  << "Vel(" << velocity.x << ", " << velocity.y << ")\n";
+    }
+
+    static double distance(const Mover& a, const Mover& b) {
+        double dx = b.position.x - a.position.x;
+        double dy = b.position.x - a.position.y;
+        return std::sqrt(dx*dx + dy*dy);
+    }
+
+    static Mover stationary(const Vec2& position) {
+        return Mover{position, Vec2::zero()};
     }
 };
 
-int main()
-{
-    Vec2 z = Vec2::zero();
-    Vec2 a(2, 2);
-    z.length();
-    z.print();
+int main() {
+    Vec2 pos(2.2, 2.99);
+    Vec2 vel(3.1, 4.1);
+
+    Mover moving(pos, vel);
+    Mover still = Mover::stationary(pos);
+
+    moving.step(1.2);
+
+    moving.print();
+    still.print();
+
+    std::cout << "distance: " << Mover::distance(moving, still) << '\n';
+
     return 0;
 }
